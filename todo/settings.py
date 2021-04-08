3c0ae4a2671f6fb0604b3bda74b7382d67e0bb81
Erick-ViBe <erickvb12@gmail.com>
2021-04-08 14:42:28 -0500

    Allow heroku host

--- todo/settings.py
+++ todo/settings.py
@@ -13,7 +13,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['todo-api-vibe.herokuapp.com']
 
 
 # Application definition
